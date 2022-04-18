4f3b5f1bde556c03ec5a6cde89003c864db44e96
aboynamedcrow <56894461+aboynamedcrow@users.noreply.github.com>
2022-04-19 03:18:16 +0800

    asdf

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -87,9 +87,9 @@
 			|| hash(password) === -1505760416
 			)}
 
-				<object title = "pdf" data="Ingwon Chae Resume 2022.03.pdf" type="application/pdf" frameborder="0" width="100%" height="1000px" style="padding: 20px;">
-    				<embed src="https://drive.google.com/file/d/1beUhXBhvxfj8fEXvHXVughxOnzv6nBrx/preview?usp=sharing" width="100%" height="600px"/> 
-				</object>
+				<!--<object title = "pdf" data="Ingwon Chae Resume 2022.03.pdf" type="application/pdf" frameborder="0" width="100%" height="1000px" style="padding: 20px;">-->
+    				<embed src="https://drive.google.com/file/d/1beUhXBhvxfj8fEXvHXVughxOnzv6nBrx/preview?usp=sharing" width="100%" height="1000px"/> 
+				<!--</object>-->
 				
 	  		{/if}
 		{/key}
